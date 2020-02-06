a6d514d3396904651c3afdb42603ede58884f0b1
roman <drsckarasu@gmail.com>
2020-02-06 21:55:05 +0200

    doing tasks

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
-var age = +prompt("Enter your age");
-var gender = prompt("Enter your gender");
+// let name = prompt("What is your name?");
+// let age = prompt("What is your age?");
+// let text = `Hello ${name}. Your age is ${age}`;
+// prompt(text);
+// let arr = [34,-23,11,1,8,29,4,6,-3];
+//
+// for (let i=0; i<arr.length; i++){
+//     if (i%2===0){
+//         alert("парне");
+//     } else{
+//         alert("непарне");
+//     }
+// }
 
-if (age>34 && gender==="female"){
-    alert("You are woman older than 34 years");
-}else if(age<34 && gender==="female"){
-    alert("Your are woman younger than 34 years")
-}else if(age>34 && gender==="male"){
-    alert("Your are man older than 34 years")
-}else if(age<34 && gender==="male"){
-    alert("Your are man younger than 34 years")
+let chambers = [["oleg","igor","eugeniy","orest"], ["lili","mila","nina"],["pasha","nick","katya"]];
+console.log(chambers);
+
+for(var i = 0; i<chambers.length; i++){
+    for(let j=0; j<chambers[i].length; j++){
+        if(chambers[i][j] === "katya"){
+            console.log("we found her in " + i+"1");
+        }
+    }
 }
