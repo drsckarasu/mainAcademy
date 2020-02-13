8358c375e9beb2fe0d3fe3312cb63d0383fbdb22
roman <drsckarasu@gmail.com>
2020-02-13 21:15:37 +0200

    updated

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
 function checkedYes(elem) {
     console.dir(elem);
     if(elem.checked === true){
-        let modelInput = document.createElement("Input");
-        modelInput.placeholder = "Car Model";
-        let modulText = document.createTextNode("Введіть марку автомобіля");
+        let noBox = document.getElementById("no");
+        if(noBox.checked === true){
+            noBox.checked = false;
+        }
         let lable = document.getElementById("carModel");
+        if (lable.hasChildNodes() === false){
+            let modelInput = document.createElement("Input");
+            modelInput.placeholder = "Car Model";
+            let modulText = document.createTextNode("Введіть марку автомобіля");
             lable.appendChild(modulText);
-            lanle.appendChild(modelInput);
+            lable.appendChild(modelInput);
+        }
     }
 }
-function f() {
-
+function checkedNo(elem) {
+    if (elem.checked === true){
+        let yesBox = document.getElementById("yes");
+        if (yesBox.checked === true){
+            yesBox.checked = false;
+            let lable = document.getElementById("carModel");
+            lable.innerHTML = "";
+            lable.removeChild(lable.firstChild);
+        }
+    }
 }
