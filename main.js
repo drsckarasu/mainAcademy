6dd45e33460f2529c8f9d90337fee7816cc3f9dd
roman <drsckarasu@gmail.com>
2020-02-15 12:54:45 +0200

    working with reqres

--- main.js
+++ main.js
@@ -1,12 +1,67 @@
-var age = +prompt("Enter your age");
-var gender = prompt("Enter your gender");
+let mainWrap = document.getElementById("mainWrapper");
+let userWrap = document.getElementById("userInfoWrap");
+getAllUsers();
 
-if (age>34 && gender==="female"){
-    alert("You are woman older than 34 years");
-}else if(age<34 && gender==="female"){
-    alert("Your are woman younger than 34 years")
-}else if(age>34 && gender==="male"){
-    alert("Your are man older than 34 years")
-}else if(age<34 && gender==="male"){
-    alert("Your are man younger than 34 years")
+function getData(url, callback){
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            let myResponse = JSON.parse(this.responseText);
+            console.dir(this.responseText);
+            callback(myResponse.data);
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+function getAllUsers() {
+    let allUsersUrl = "https://reqres.in/api/users?page=1";
+    getData(allUsersUrl, createUsersList);
+}
+function getUser(id) {
+    let oneUser = "https://reqres.in/api/users/"+id;
+    getData(oneUser, createUserInfo);
+}
+
+function createUsersList(users) {
+     for(let i=0; i<users.length; i++){
+        let elementWrap = document.createElement("div");
+        elementWrap.className = "elementWr";
+
+        let nameField = document.createElement("div");
+        nameField.innerHTML = users[i].first_name + " " + users[i].last_name;
+        let emailField = document.createElement("div");
+        emailField.innerHTML = users[i].email;
+        let imageField = document.createElement("img");
+        imageField.src = users[i].avatar;
+        
+        let moreButton = document.createElement("button");
+        moreButton.id = users[i].id;
+        moreButton.addEventListener("click", function () {
+            moreInfo(this);
+        });
+        moreButton.innerHTML = "More Info";
+        moreButton.className = "btn";
+
+        elementWrap.appendChild(imageField);
+        elementWrap.appendChild(nameField);
+        elementWrap.appendChild(emailField);
+        elementWrap.appendChild(moreButton);
+        mainWrap.appendChild(elementWrap);
+    }
+}
+function moreInfo(event) {
+    mainWrap.style.display = "none";
+    getUser(event.id);
+}
+function createUserInfo(user) {
+    let nameField = document.createElement("div");
+    nameField.innerHTML = user.first_name + " " + user.last_name;
+    let emailField = document.createElement("div");
+    emailField.innerHTML = user.email;
+    let imageField = document.createElement("img");
+    imageField.src = user.avatar;
+    userWrap.appendChild(imageField);
+    userWrap.appendChild(nameField);
+    userWrap.appendChild(emailField);
 }
